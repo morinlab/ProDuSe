f6f1f6e10787a96e2d40a1b91e32a7586906b99a
marcoalbuquerque-sfu <marcoalbuquerque.sfu@gmail.com>
2015-12-22 10:30:48 -0800

    Add overlapping read filter and finds read pairs properly

--- position.py
+++ position.py
@@ -2,8 +2,10 @@ import pysam
 import bisect
 import nucleotide
 import collections
-import fasta
+import sys
+import itertools
 from collections import Counter
+
 
 class ID:
 
@@ -22,6 +24,21 @@ class Tally:
 
     def __str__(self):
         return ':'.join([str(self.a), str(self.c), str(self.g), str(self.t)]);
+
+    def get_count(self, base):
+        if base == 'A':
+            return self.a
+        elif base == 'C':
+            return self.c
+        elif base == 'G':
+            return self.g
+        elif base == 'T':
+            return self.t
+        else:
+            sys.exit("Base not available in Tally")
+
+    def sum(self):
+        return (self.a + self.c + self.g + self.t);
 
 
 class Order:
@@ -55,6 +72,7 @@ class Order:
     def __str__(self):
         return ':'.join([str(self.chrom), str(self.start)])
 
+
 class Pos:
 
     def __init__(self, base, qual, qname):
@@ -65,6 +83,14 @@ class Pos:
         self.start = int(split[2])
         self.end = int(split[4])
         self.sense = split[6]
+        self.count = int(split[5])
+
+    def equal_coords(self, other):
+        if self.start == other.start and self.end == other.end:
+            return True
+        else:
+            return False
+
 
     def __eq__(self, other):
         if self.start == other.start and self.end == other.end and self.base == other.base and self.sense != other.sense:
@@ -107,46 +133,78 @@ class PosCollection:
         self.pos_bases = []
         self.neg_bases = []
         self.true_bases = []
+  
 
+    def find_true_bases(self, adapter_sequence, max_mismatch):
 
-    def which_equal(self):
-        which = []
-        if len(self.list_of_pos) == 1:
-            return which
+        pos_set = set();
+        neg_set = set();
+        positive = [];
+        negative = [];
+        start = self.list_of_pos[0].start;
+        end = self.list_of_pos[0].end;
+        bases = [];
+        score = [];
+        indexes = nucleotide.which_ambiguous(adapter_sequence);
+        outputs = [];
 
-        else:
-            for i in range(len(self.list_of_pos[:-1])):
-                if (self.list_of_pos[i] == self.list_of_pos[i+1]):
-                    which.append(i);
-            return which    
+        for pos in self.list_of_pos:
 
+            if pos.start != start or pos.end != end:
 
-    def collect_true_bases(self, which, adapter_sequence, max_mismatch):
+                if len(positive) > 0 and len(negative) > 0:
+                    for i in positive:
+                        for j in negative:
+                            if nucleotide.distance(i.adapter, j.adapter, indexes) <= max_mismatch:
+                                if i.base == j.base:
+                                    bases.append(i.base);
 
-        for i in which:
-            indexes = nucleotide.which_ambiguous(adapter_sequence)
-            if nucleotide.distance(self.list_of_pos[i].adapter, self.list_of_pos[i+1].adapter, indexes) <= max_mismatch :
-                self.true_bases.append(self.list_of_pos[i].base);
+                positive = [];
+                negative = [];
+                pos_set = set();
+                neg_set = set();
+                start = pos.start;
+                end = pos.end;
 
+            if pos.sense == "+":
+                positive.append(pos);
 
-    def is_variant(self, adapter_sequence, max_mismatch):
+            elif pos.sense == "-":
+                negative.append(pos);
+
+        return bases;
+             
+
+    def is_variant(self, adapter_sequence, max_mismatch = 3, alt_base_count_threshold = 5, strand_bias_threshold = 0.2, variant_allele_fraction_threshold = 0.1):
 
         self.neg_bases = [ pos.base if pos.sense == "-" else None for pos in self.list_of_pos ];
         self.pos_bases = [ pos.base if pos.sense == "+" else None for pos in self.list_of_pos ];
-        self.all_bases = self.neg_bases + self.pos_bases;
-        which = self.which_equal();
+        self.neg_tally = Tally(self.neg_bases);
+        self.pos_tally = Tally(self.pos_bases);
+        self.all_bases = [ pos.base for pos in self.list_of_pos ];
+        self.norm_neg_tally = Tally(list(itertools.chain.from_iterable( [ [pos.base] * pos.count if pos.sense == "-" else [] for pos in self.list_of_pos ] )))
+        self.norm_pos_tally = Tally(list(itertools.chain.from_iterable( [ [pos.base] * pos.count if pos.sense == "+" else [] for pos in self.list_of_pos ] )))
+        self.true_bases = self.find_true_bases(adapter_sequence, max_mismatch);
 
-        if not len(which) == 0: 
-            self.collect_true_bases(which, adapter_sequence, max_mismatch);
-            if len(self.true_bases) > 0:
-                if not all(x == self.true_bases[0] for x in self.true_bases):
-                    tmp = Counter(self.true_bases);
-                    most = tmp.most_common(2);
-                    if most[0][0] == self.ref:
-                        self.alt = most[1][0];
-                    else:
-                        self.alt = most[0][0];
+        if not len(self.true_bases) == 0: 
+            if not all(x == self.true_bases[0] for x in self.true_bases):
+                tmp = Counter(self.true_bases);
+                most = tmp.most_common(2);
+                if most[0][0] == self.ref:
+                    self.alt = most[1][0];
+                else:
+                    self.alt = most[0][0];
+
+                self.alt_count = sum([ True if x == self.alt else False for x in self.true_bases]);
+                self.ref_count = sum([ True if x == self.ref else False for x in self.true_bases]);
+
+                if self.neg_tally.get_count(self.alt) >= alt_base_count_threshold and  \
+                   self.pos_tally.get_count(self.alt) >= alt_base_count_threshold and \
+                   float(min(self.neg_tally.sum(),self.pos_tally.sum())) / float(self.neg_tally.sum() + self.pos_tally.sum()) >= strand_bias_threshold and \
+                   float(self.alt_count) / float(self.ref_count + self.alt_count) >= variant_allele_fraction_threshold:                       
                     return True;
+                else:
+                    return False;
         else:
             return False
 
@@ -155,8 +213,8 @@ class PosCollection:
 
         return '\t'.join([
             str(self.chrom),
-            str(self.start),
-            str(self.start),
+            str(self.start + 1),
+            str(self.start + 1),
             '/'.join([
                 self.ref,
                 self.alt
@@ -164,18 +222,23 @@ class PosCollection:
             "1",
             str(Tally(self.true_bases)),
             str(Tally(self.pos_bases)),
-            str(Tally(self.neg_bases))
+            str(Tally(self.neg_bases)),
+            str(self.norm_pos_tally),
+            str(self.norm_neg_tally)
             ]);
 
 class PosCollectionCreate:
 
-    def __init__(self, pysam_alignment_file, pysam_fasta_file):
+    def __init__(self, pysam_alignment_file, pysam_fasta_file, filter_overlapping_reads = True):
         self.pysam_alignment_file = pysam_alignment_file
         self.pysam_fasta_file = pysam_fasta_file
         self.pos_collections = {}
+        self.filter_overlapping_reads = filter_overlapping_reads
+        self.qname_collections = {}
         self.base_buffer = 5000
         self.order = []
         self.read_processed = False
+        self.qnames = {};
 
     def __iter__(self):
         return self.next()
@@ -207,9 +270,15 @@ class PosCollectionCreate:
 
                 if not order in self.pos_collections:
                     self.pos_collections[order] = []
+                    self.qname_collections[order] = {}
                     bisect.insort(self.order, order)
+                
+                if not self.filter_overlapping_reads:
+                    bisect.insort(self.pos_collections[order], current_pos)
 
-                bisect.insort(self.pos_collections[order], current_pos)
+                elif read.qname not in self.qname_collections[order]:
+                    self.qname_collections[order][read.qname] = True
+                    bisect.insort(self.pos_collections[order], current_pos)
 
 
             # Blah Blah
@@ -226,4 +295,5 @@ class PosCollectionCreate:
                     )
 
                 del self.pos_collections[self.order[0]]
+                del self.qname_collections[self.order[0]]
                 del self.order[0]
