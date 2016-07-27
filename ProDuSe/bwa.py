cb8c5443dd8815f3dfc6966fb34ed4a1ada47513
marcoalbuquerque-sfu <marcoalbuquerque.sfu@gmail.com>
2016-07-27 13:37:59 -0700

    Add configuration files for bwa.py

--- ProDuSe/bwa.py
+++ ProDuSe/bwa.py
@@ -1,48 +1,74 @@
-import argparse
-import printer
+import configargparse
 import sys
-import select
 import os
-import signal
 import subprocess
+import time
+import itertools
 
 if __name__ == '__main__':
 
-    printer.general('BWA')
-
     desc = "Align Paired Reads for the PRODUSE Pipeline"
-    parser = argparse.ArgumentParser(description=desc)
-    parser.add_argument(
-        "-f", "--fastqs",
-        nargs=2,
-        required=True,
-        help="Input Fastqs"
+    parser = configargparse.ArgParser(description=desc)
+    parser.add(
+        "-c", "--config",
+        required=False,
+        is_config_file=True,
+        help="An optional configuration file for any of the input arguments."
         )
-    parser.add_argument(
-        "-b", "--bam",
-        nargs=1,
+    parser.add(
+        "-i", "--input",
         required=True,
-        help="Output Sorted Bam"
+        action="append",
+        type=str,
+        help="A pair of fastq files for reading processed by trim.py"
         )
-    parser.add_argument(
+    parser.add(
+        "-o", "--output",
+        required=True,
+        type=str,
+        help="An output bam file for writing"
+        )
+    parser.add(
         "-r", "--reference",
-        nargs=1,
         required=True,
-        help="BWA Reference"
+        type=str,
+        help="A genome reference file with BWA indexes"
         )
+
     args = parser.parse_args()
 
+    if not len(args.input) == 2:
+        parser.error('--input must be a pair (i.e. a sized two list) of fastq files')
+
+    print_prefix = "PRODUSE-BWA        " ;
+    sys.stdout.write(print_prefix + time.strftime('%X') + "    " + "Starting...\n")    
+
+    if not os.path.isfile(args.input[0]):
+        sys.stdout.write(print_prefix + time.strftime('%X') + "    " + "Error: input fastq files does not exist - " + args.input[0] + "\n")
+        sys.exit(1)
+
+    if not os.path.isfile(args.input[1]):
+        sys.stdout.write(print_prefix + time.strftime('%X') + "    " + "Error: input fastq files does not exist - " + args.input[1] + "\n")
+        sys.exit(1)       
+
     output_prefix = ""
-    if args.bam[0][-4:] == ".bam":
-        output_prefix = args.bam[0][:-4]
+    resulting_file = ""
+    if args.output[-4:] == ".bam":
+        output_prefix = args.output[:-4]
+        resulting_file = args.output
     else:
-        output_prefix = args.bam[0]
+        output_prefix = args.output
+        resulting_file = args.output + ".bam"
+
+    if os.path.isfile(resulting_file) :
+        sys.stdout.write(print_prefix + time.strftime('%X') + "    " + "Error: output bam file already exists - " + resulting_file + "\n")
+        sys.exit(1)
 
     command1 = [
         'bwa', 'mem',
-        args.reference[0],
-        args.fastqs[0],
-        args.fastqs[1]
+        args.reference,
+        args.input[0],
+        args.input[1]
         ]
 
     command2 = [
@@ -55,22 +81,19 @@ if __name__ == '__main__':
         "-", output_prefix
         ]
 
-    ps1 = subprocess.Popen(command1, stdout=subprocess.PIPE) #, stderr=subprocess.PIPE)
-    ps2 = subprocess.Popen(command2, stdout=subprocess.PIPE, stdin=ps1.stdout) #, stderr=subprocess.PIPE)
-    ps3 = subprocess.Popen(command3, stdin=ps2.stdout) #, stderr=subprocess.PIPE)
+    ps1 = subprocess.Popen(command1, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    ps2 = subprocess.Popen(command2, stdout=subprocess.PIPE, stdin=ps1.stdout, stderr=subprocess.PIPE)
+    ps3 = subprocess.Popen(command3, stdin=ps2.stdout, stderr=subprocess.PIPE)
 
-    #ps1_iterator = iter(ps1.stderr.readline, b"")
-    #ps2_iterator = iter(ps2.stderr.readline, b"")
-    #ps3_iterator = iter(ps3.stderr.readline, b"")
+    ps1_iterator = iter(ps1.stderr.readline, b"")
+    ps2_iterator = iter(ps2.stderr.readline, b"")
+    ps3_iterator = iter(ps3.stderr.readline, b"")
 
-    #counter = 0
-    #printer.general(" ".join(["Reads Aligned:", str(counter)]))
-
-    #for line in ps1_iterator:
- 
-    #    if line.startswith("[M::main_mem]"):
-    #        counter += int(line.split()[2])
-    #        printer.general(" ".join(["Reads Aligned:", str(counter)]))
+    counter = 0 
+    for line in itertools.chain(ps1_iterator, ps2_iterator, ps3_iterator):
+        if line.startswith("[M::mem_process_seqs]"):
+            counter += int(line.split(" ")[2])
+            sys.stdout.write(print_prefix + time.strftime('%X') + "    " + "Reads Processed:" + str(counter) + "\n")
 
     ps1.stdout.close()
     ps2.stdout.close()
