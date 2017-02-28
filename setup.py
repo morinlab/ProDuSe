7f2a17b922ba05f2f05cc05efbfa9f1ea27f2870
Christopher Rushton <ckrushto@sfu.ca>
2017-02-28 09:13:39 -0800

    Modified install to include perl scripts, work with new workflow
    Perl scripts are now included in the 'ProDuSe' directory during install
    Readme and Liscence are now included in the 'ProDuSe' directory during install
    'ProdusePipeline' can now by run from the installation using 'produse run_produse'
    Renamed 'produce_pipeline' to 'configure_produse'

--- setup.py
+++ setup.py
@@ -1,21 +1,27 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from distutils.command.install import INSTALL_SCHEMES
+
+for scheme in INSTALL_SCHEMES.values():
+    scheme['data'] = scheme['purelib']
 
 setup(
     name='ProDuSe',
-    version="0.1.6",
-    description='Process Duplex Sequence',
+    version="0.1.7",
+    description='Process Duplex Sequence Data',
     author='Marco Albuquerque',
     author_email='malbuque@sfu.ca',
+    maintainer='Christopher Rushton',
+    maintainer_email='ckrushto@sfu.ca',
     packages=["ProDuSe"],
     url='https://github.com/morinlab/ProDuSe',
     classifiers=[
        "Programming Language :: Python :: 2.7"
        ],
-    download_url="https://github.com/morinlab/ProDuSe/tarball/0.1.6",
+    download_url="https://github.com/morinlab/ProDuSe/dist/ProDuse-0.1.7",
     license="GNU GPLv3",
     scripts=["bin/produse"],
-    data_files = ["LICENSE.txt", "README.md"]
+    data_files = [("ProDuSe", ["LICENSE.txt", "README.md", "ProDuSe/splitmerge.pl", "ProDuSe/filter_produse.pl"])]
 )
 
