1a910bfda09f3da22da3a76729b1cbf671ac905b
Christopher Rushton <ckrushto@sfu.ca>
2017-02-28 09:17:56 -0800

    Modified argument parsing to allow script to be run using 'produse'

--- ProDuSe/ProdusePipeline.py
+++ ProDuSe/ProdusePipeline.py
@@ -3,20 +3,18 @@
 import argparse
 import os
 import subprocess
-# import glob
 import sys
 
 cwd = os.path.abspath(".") + os.sep
 scriptLoc = os.path.abspath(os.path.dirname(__file__)) + os.sep
-# stitcherDefault = glob.glob(scriptLoc + os.sep + "Pisces*" + os.sep + "redist" + os.sep + "Stitcher.exe")[0]
 
 parser = argparse.ArgumentParser(description="Runs all stages of the ProDuce pipeline on the provided samples")
 parser.add_argument("-sc", "--sampleconfig", required=True, type=lambda x: isValidFile(x), help="Sample Configuration File")
 parser.add_argument("-c", "--produceconfig", required=True, type=lambda x: isValidFile(x), help="Produce Configuration File")
-parser.add_argument("-r", "--refgenome", required=True, type=lambda x: isValidFile(x), help="Reference genome, in FASTA format")
+parser.add_argument("-r", "--refgenome", required=True, type=lambda x: isValidFile(x), help="Reference genome with BWA index, in FASTA format")
 parser.add_argument("-d", "--outdir", default=cwd, help="Output directory [Default: %(default)s]")
 parser.add_argument("-e", "--scriptdir", default=scriptLoc, help="Directory containing Produce Scripts [Default: %(default)s]")
-parser.add_argument("-x", "--stitcherpath", required=True, help="Path to Stitcher.exe [Default: %(default)s]")
+parser.add_argument("-x", "--stitcherpath", required=True, type=lambda x: isValidFile(x), help="Path to Stitcher.exe [Default: %(default)s]")
 parser.add_argument("-t", "--threads", default=1, type=int, help="Number of threads to use while running BWA [Default: %(default)s]")
 parser.add_argument("-v", "--vaf", default=0.05, type=float, help="Variant Allele Fraction (VAF) threshold used in variant filtering [Default: %(default)s]")
 
@@ -26,8 +24,8 @@ def isValidFile(file):
 	if os.path.exists(file):
 		return file
 	else:
-		print("ERROR: Unable to locate " + file)
-		print("Please ensure the file exists")
+		sys.stderr.write("ERROR: Unable to locate %s\n" % file)
+		sys.stderr.write("Please ensure the file exists\n")
 		exit()
 
 
@@ -55,8 +53,8 @@ def runConfig(sConfig, pConfig, refGenome, outDir, scriptDir):
 
 	scriptPath = scriptDir + os.sep + "configure_produse.py"
 	if not os.path.exists(scriptPath):
-		print("ERROR: Unable to locate configure_produce.py in " + scriptDir + os.sep)
-		print("Please ensure the script exists")
+		sys.stderr.write("ERROR: Unable to locate configure_produce.py in %s\n" % (scriptDir))
+		sys.stderr.write("Please ensure the script exists\n")
 		exit(1)
 
 	configArgs = ["python", scriptPath, "-o", outDir + os.sep, "-c", pConfig, "-sc", sConfig, "-r", refGenome]
@@ -67,7 +65,7 @@ def getSampleList(outDir):
 
 	# Generates a list of samples by listing all directoies created in 'produce_analysis_directory'
 	if not os.path.exists(outDir + os.sep + "produse_analysis_directory" + os.sep):
-		print("ERROR: configure_produce.py did not complete sucessfully")
+		sys.stderr.write("ERROR: configure_produce.py did not complete sucessfully\n")
 		exit(1)
 
 	sampleList = next(os.walk(outDir + os.sep + "produse_analysis_directory" + os.sep))[1]
@@ -82,12 +80,12 @@ def processSample(sample, sampleDir, scriptDir, threads, stitcherPath, vaf):
 		trimConfig = sampleDir + os.sep + "config" + os.sep + "trim_task.ini"
 
 		if not os.path.exists(scriptPath):
-			print("ERROR: Unable to locate \'trim.py\'' in " + scriptDir)
-			print("Please ensure the script exists")
+			sys.stderr.write("ERROR: Unable to locate \'trim.py\'' in %s\n" % (scriptDir))
+			sys.stderr.write("Please ensure the script exists\n")
 			exit(1)
 		elif not os.path.exists(trimConfig):
-			print("ERROR: Unable to locate \'trim_task.ini\' in " + sampleDir)
-			print("Ensure that \'configure_produce.py\' completed successfully")
+			sys.stderr.write("ERROR: Unable to locate \'trim_task.ini\' in %s\n" % sampleDir)
+			sys.stderr.write("Ensure that \'configure_produce.py\' completed successfully\n")
 			exit(1)
 
 		trimArgs = ["python", scriptPath, "-c", trimConfig]
@@ -97,12 +95,12 @@ def processSample(sample, sampleDir, scriptDir, threads, stitcherPath, vaf):
 
 		scriptPath = scriptDir + os.sep + "bwa.py"
 		if not os.path.exists(scriptPath):
-			print("ERROR: Unable to locate \'bwa.py\'' in " + scriptDir)
-			print("Please ensure the script exists")
+			sys.stderr.write("ERROR: Unable to locate \'bwa.py\'' in %s\n" % (scriptDir))
+			sys.stderr.write("Please ensure the script exists\n")
 			exit(1)
 		elif not os.path.exists(config):
-			print("ERROR: Unable to locate " + config)
-			print("Ensure that \'configure_produce.py\' completed successfully")
+			sys.stderr.write("ERROR: Unable to locate %s\n" % (config))
+			sys.stderr.write("Ensure that \'configure_produce.py\' completed successfully\n")
 			exit(1)
 
 		bwaArgs = ["python", scriptPath, "-c", config, "-t", str(threads)]
@@ -114,12 +112,12 @@ def processSample(sample, sampleDir, scriptDir, threads, stitcherPath, vaf):
 		collapseConfig = sampleDir + os.sep + "config" + os.sep + "collapse_task.ini"
 
 		if not os.path.exists(scriptPath):
-			print("ERROR: Unable to locate \'collapse.py\'' in " + scriptDir)
-			print("Please ensure the script exists")
+			sys.stderr.write("ERROR: Unable to locate \'collapse.py\'' in %s\n" % (scriptDir))
+			sys.stderr.write("Please ensure the script exists\n")
 			exit(1)
 		elif not os.path.exists(collapseConfig):
-			print("ERROR: Unable to locate \'collapse_task.ini\' in " + sampleDir)
-			print("Ensure that \'configure_produce.py\' completed successfully")
+			sys.stderr.write("ERROR: Unable to locate \'collapse_task.ini\' in %s\n" % (sampleDir))
+			sys.stderr.write("Ensure that \'configure_produce.py\' completed successfully\n")
 			exit(1)
 
 		collapseArgs = ["python", scriptPath, "-c", collapseConfig]
@@ -128,8 +126,8 @@ def processSample(sample, sampleDir, scriptDir, threads, stitcherPath, vaf):
 	def runStitcher(sampleDir, stitcherPath):
 
 		if not os.path.exists(stitcherPath):
-			print("ERROR: Unable to locate " + stitcherPath)
-			print("Please ensure the executable exists")
+			sys.stderr.write("ERROR: Unable to locate %s\n" % stitcherPath)
+			sys.stderr.write("Please ensure the executable exists\n")
 			exit(1)
 
 		bamFile = sampleDir + os.sep + "data" + os.sep + "collapse.bam"
@@ -149,8 +147,8 @@ def processSample(sample, sampleDir, scriptDir, threads, stitcherPath, vaf):
 
 		scriptPath = scriptDir + os.sep + "splitmerge.pl"
 		if not os.path.exists(scriptPath):
-			print("ERROR: Unable to locate \'splitmerge.pl\' in " + scriptDir)
-			print("Please ensure the script exists")
+			sys.stderr.write("ERROR: Unable to locate \'splitmerge.pl\' in %s\n" % scriptDir)
+			sys.stderr.write("Please ensure the script exists\n")
 
 		preSMViewArgs = ["samtools", "view", "-h", bamFile]
 		postSMViewArgs = ["samtools", "view", "-b"]
@@ -177,39 +175,39 @@ def processSample(sample, sampleDir, scriptDir, threads, stitcherPath, vaf):
 			subprocess.Popen(filterArgs, stdin=f, stdout=o)
 
 	runTrim(sampleDir, scriptDir)
-	print(sample + ": Trimming Complete")
+	sys.stderr.write(sample + ": Trimming Complete\n")
 
 	trimBwaConfig = sampleDir + os.sep + "config" + os.sep + "trim_bwa_task.ini"
 	runBWA(trimBwaConfig, scriptDir, threads)
-	print(sample + ": Mapping Complete")
+	sys.stderr.write(sample + ": Mapping Complete\n")
 
 	runCollapse(sampleDir, scriptDir)
-	print(sample + ": Collapsing Complete")
+	sys.stderr.write(sample + ": Collapsing Complete\n")
 
 	collapseBwaConfig = sampleDir + os.sep + "config" + os.sep + "collapse_bwa_task.ini"
 	runBWA(collapseBwaConfig, scriptDir, threads)
-	print(sample + ": Mapping Complete")
+	sys.stderr.write(sample + ": Mapping Complete\n")
 
 	runStitcher(sampleDir, stitcherPath)
-	print(sample + ": Stitcher Complete")
+	sys.stderr.write(sample + ": Stitcher Complete\n")
 
 	sortedStitchBAM = runSort(sampleDir + os.sep + "data" + os.sep + "collapse.stitched.bam")
 
 	splitMergeName = sampleDir + os.sep + "results" + os.sep + "SplitMerge.bam"
 	runSplitMerge(splitMergeName, sortedStitchBAM, scriptDir)
-	print(sample + ": SplitMerge Complete")
+	sys.stderr.write(sample + ": SplitMerge Complete\n")
 
-	print(sample + ": Calling SNVs...")
+	sys.stderr.write(sample + ": Calling SNVs...\n")
 	runSort(splitMergeName)
 	snvConfig = sampleDir + os.sep + "config" + os.sep + "snv_task.ini"
 	runSNV(snvConfig, sampleDir, scriptDir)
-	print(sample + ": SNV Calling Complete")
+	sys.stderr.write(sample + ": SNV Calling Complete\n")
 
 	vcfFile = sampleDir + os.sep + "results" + os.sep + "variants.vcf"
 	runFilter(scriptDir, vaf, vcfFile)
-	print(sample + ": Filtering Complete")
+	sys.stderr.write(sample + ": Filtering Complete\n")
 
-	print(sample + ": ProDuSe Complete. Output in " + sampleDir + os.sep + "results" + os.sep)
+	sys.stderr.write(sample + ": ProDuSe Complete. Output in " + os.path.join(sampleDir, "results") + "\n")
 
 
 def main(args=None):
@@ -217,22 +215,23 @@ def main(args=None):
 	if args is None:
 		args = parser.parse_args()
 
+	sys.stderr.write("Processing...\n")
 	if not os.path.isfile(args.stitcherpath):
 
-		print("ERROR: Unable to locate Stitcher.exe in " + os.path.dirname(args.stitcherpath))
+		sys.stderr.write("ERROR: Unable to locate Stitcher.exe in %s\n" % os.path.dirname(args.stitcherpath))
 		exit(1)
 
-	createLogFile(args, args.outdir + os.sep + "ProDuSe_Task.log")
+	createLogFile(args, os.path.abspath(args.outdir) + os.sep + "ProDuSe_Task.log")
 	runConfig(args.sampleconfig, args.produceconfig, args.refgenome, args.outdir, args.scriptdir)
 	sampleList = getSampleList(args.outdir)
 
 	for sample in sampleList:
 
-		print("Processing sample " + sample)
+		sys.stderr.write("Processing sample %s\n" % (sample))
 		sampleDir = args.outdir + os.sep + "produse_analysis_directory" + os.sep + sample
 		processSample(sample, sampleDir, args.scriptdir, args.threads, args.stitcherpath, args.vaf)
 
-	print("ProDuSe Complete. All samples processed")
+	sys.stderr.write("All samples processed. ProDuSe complete.\n")
 
 
 if __name__ == "__main__":
