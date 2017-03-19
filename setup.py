8ff764cf17b6472f4fcb6e630e2afc48a7f8f595
Christopher Rushton <ckrushto@sfu.ca>
2017-03-19 14:16:50 -0700

    Updated version to 0.2.1

--- setup.py
+++ setup.py
@@ -4,14 +4,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='ProDuSe',
-    version="0.1.7",
+    version="0.2.1",
     description='Process Duplex Sequence Data',
     author='Marco Albuquerque',
     author_email='malbuque@sfu.ca',
     maintainer='Christopher Rushton',
     maintainer_email='ckrushto@sfu.ca',
     include_package_data=True,
-    # install_requires=["samtools >= 1.0"],
     packages=["ProDuSe"],
     url='https://github.com/morinlab/ProDuSe',
     classifiers=[
@@ -25,7 +24,7 @@ setup(
         "pysam",
         "numpy"
         ],
-    download_url="https://github.com/morinlab/ProDuSe/dist/ProDuse-0.1.7",
+    download_url="https://github.com/morinlab/ProDuSe/dist/ProDuSe-0.18.0.tar.gz",
     license="GNU GPLv3",
     scripts=["bin/produse"],
     data_files = [("ProDuSe", ["LICENSE.txt", "README.md", "ProDuSe/splitmerge.pl", "ProDuSe/filter_produse.pl"])]
